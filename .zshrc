4821e78aac649dd61d32ba7f1d20530b3a54d854
Paolo Laurenti <laurentipaolo@gmail.com>
2017-06-17 13:22:17 +0200

    Add script for automatic configuration

--- .zshrc
+++ .zshrc
@@ -1,24 +1,15 @@
-source $HOME/dot-files/antigen/antigen.zsh
+source $HOME/dotfiles/antigen.zsh
 
 # Load the oh-my-zsh's library.
 antigen use oh-my-zsh
 
 # Bundles from the default repo (robbyrussell's oh-my-zsh).
 antigen bundle git
+antigen bundle heroku
 antigen bundle fabiokiatkowski/exercism.plugin.zsh
-#antigen bundle command-not-found
 
 # Load the theme.
-antigen-theme pygmalion
+antigen theme pygmalion
 
 # Tell antigen that you're done.
 antigen apply
-
-source $HOME/.cred.sh
-
-PATH=/usr/local/bin:$PATH
-
-. $HOME/.asdf/asdf.sh
-. $HOME/.asdf/completions/asdf.bash
-
-alias gitconfig='/usr/local/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
---
+++ install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DOT_FILES_DIR="$( cd "$( dirname "$0" )" && pwd )"
+
+echo "setup zsh"
+
+if [ "$SHELL" = "/bin/zsh" ]
+then
+  echo "zsh is already the default shell"
+else
+  echo "check if zsh is already installed"
+
+  if which "zsh"
+  then
+    echo "zsh is already installed"
+  else
+    echo "zsh is not installed yet..."
+    echo ""
+    sudo apt-get install zsh
+    echo ""
+    echo "zsh has been installed"
+  fi
+
+  echo "change default shell to zsh"
+
+  if grep -q "zsh" /etc/shells
+  then
+    echo "zsh is already an authorized shell"
+  else
+    echo "make zsh an authorized shell"
+    command -v zsh | sudo tee -a /etc/shells
+  fi
+
+  chsh -s $(which zsh)
+
+  echo "zsh is now the default shell"
+  echo "Please, log off and log in"
+fi
+
+echo "make links between $DOT_FILES_DIR and $HOME"
+rm -f ~/.zshrc && ln -sf "$DOT_FILES_DIR/.zshrc" ~/.zshrc
+
+echo "done"
